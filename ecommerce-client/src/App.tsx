a9c6100140d658bba742087ae58d76b10d68d766
Haadi Feisal <haadii_94@hotmail.com>
2022-11-27 21:07:41 +0100

    * Added fetch api call.

--- ecommerce-client/src/App.tsx
+++ ecommerce-client/src/App.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [products, setProducts] = useState([
+      {
+        name: "Fifa 23",
+        price: 799
+      },
+      {
+        name: "Xbox 360",
+        price: 2999
+      }
+  ]);
+
+  useEffect(() => {
+    fetch('https://localhost:44374/api/Product')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  function addProductHandler(){
+    setProducts([...products, {name: "GTA VI", price: 599}]);
+  }
+
   return (
-    <div>
-      <h1>Haadi Feisal</h1>
+    <div className="app">
+      {products.map((product, index) => (
+        <h1>{product.name} {index}</h1>
+      ))}
+      <button onClick={addProductHandler}>Add Product</button>
     </div>
   );
 }
