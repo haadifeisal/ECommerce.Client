07e5120ba0ccd4b6bfe9310a54247517a5d1835b
Haadi Feisal <haadii_94@hotmail.com>
2022-12-13 10:21:49 +0100

    routes

--- ecommerce-client/src/app/layout/App.tsx
+++ ecommerce-client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { Container, createTheme, ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from "./Header";
 
 function App() {
@@ -26,7 +31,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog></Catalog>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/catalog' element={<Catalog/>} />
+          <Route path='/catalog/:id' element={<ProductDetails/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/contact' element={<ContactPage/>} />
+        </Routes>
       </Container>
     </ThemeProvider>
   );
